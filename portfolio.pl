f8c38a89e7ddd467bf56a54e646fb52964a37b23
Grygoree <GregoryLeung2016@u.northwestern.edu>
2015-11-06 21:20:54 -0600

    fucking perl

--- portfolio.pl
+++ portfolio.pl
@@ -1,3 +1,28 @@
 #!/usr/bin/perl -w
 
-print "Hello, World!";
+use strict;
+
+
+
+
+use CGI qw(:standard);
+use URI::Escape;
+use Switch;
+use Scalar::Util;
+
+
+use DBI;
+
+use Time::ParseDate;
+
+print "<html>";
+
+print "<head>";
+
+print "</head>";
+
+print "<body>";
+
+print "</body>";
+
+print "</html>";
