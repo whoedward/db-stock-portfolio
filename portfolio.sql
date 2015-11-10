c86be922dfd931e5885aded9c022b51315ba9f23
Edward Hu <ehe839@murphy.wot.ece.northwestern.edu>
2015-11-09 20:38:56 -0600

    asdf

--- portfolio.sql
+++ portfolio.sql
@@ -8,14 +8,18 @@ create table portfolio_portfolio(
   constraint portfolio_pk primary key (id, owner),
   balance number not null
 );
+create table portfolio_stock_symbols(
+  name varchar(16) unique not null
+);
 create table portfolio_stock_portfolio(
   owner varchar(64) not null references portfolio_users(name),
 --TODO: find out how long stock key can be
-  stock varchar(16) not null,
+  stock varchar(16) not null references portfolio_stock_symbols(name),
   owned number not null,
   constraint name unique (owner, stock)
 );
 
+INSERT all into portfolio_stock_symbols(name) SELECT unique symbol FROM cs339.StocksDaily;
 --INSERT into portfolio_users (name, password, balance) VALUES ('anon', 'anonanon', 0);
 --Test users--
 INSERT into portfolio_users (name, password) VALUES ('poorjoe','poorjoe');
