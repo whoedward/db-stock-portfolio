ca12e0b52453dc60e2042c15bcbe146edeb6c537
Grygoree <GregoryLeung2016@u.northwestern.edu>
2015-11-12 18:35:50 -0600

    finalized portfolio-drop.sql

--- portfolio-drop.sql
+++ portfolio-drop.sql
@@ -1,11 +1,9 @@
-delete from portfolio_stock_portfolio;
+delete from portfolio_stock_holding;
 delete from portfolio_portfolio;
 delete from portfolio_users;
-delete from portfolio_stock_symbols;
 
 commit;
 
-drop table portfolio_stock_portfolio;
+drop table portfolio_stock_holding;
 drop table portfolio_portfolio;
 drop table portfolio_users;
-drop table portfolio_stock_symbols;
